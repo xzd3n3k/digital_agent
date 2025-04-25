8299e402383c63988a9c9e4d69ee4c41ddcec5aa
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-04-25 17:01:19 +0200

    add icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,2 +1,5 @@
 export { ReactComponent as CloseIcon } from './close_icon.svg';
 export { ReactComponent as MenuIcon } from './menu_icon.svg';
+export { ReactComponent as CoinsIcon } from './coins_icon.svg';
+export { ReactComponent as ChartLineUpIcon } from './chart_line_up_icon.svg';
+export { ReactComponent as ShieldCheckIcon } from './shield_check_icon.svg';
