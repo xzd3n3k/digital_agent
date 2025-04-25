bd401e2bb7dbae0f0fc1199ccde2998119e37de8
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-04-25 17:01:51 +0200

    add statsshowcase component to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import './Home.scss';
 import React from "react";
-import {Heading} from "../../components";
+import {Heading, StatsShowcase} from "../../components";
 
 export default function Home() {
     console.log("\n\n{\\__/}\n" +
@@ -19,6 +19,14 @@ export default function Home() {
     return (
         <div className="home-container">
             <Heading>Solving problems. Thousands at a time</Heading>
+            <span>It is a long <b>established fact that a reader</b> will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal <b>distribution of letters</b>, as opposed to using 'Content here, content here', making it look like readable English.</span>
+            <StatsShowcase stats={
+                [
+                    { icon: "CoinsIcon", text: "Reduce costs by 40%." },
+                    {icon: "ChartLineUpIcon", text: "Increase customer satisfaction by 30%."},
+                    {icon: "ShieldCheckIcon", text: "Trusted by those you know."}
+                ]
+            }></StatsShowcase>
         </div>
     )
 }
