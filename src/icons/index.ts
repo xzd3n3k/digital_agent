db492ce26c1755d7ca23466dc58cdd119a84b39a
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-04-27 12:19:30 +0200

    add icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,3 +3,5 @@ export { ReactComponent as MenuIcon } from './menu_icon.svg';
 export { ReactComponent as CoinsIcon } from './coins_icon.svg';
 export { ReactComponent as ChartLineUpIcon } from './chart_line_up_icon.svg';
 export { ReactComponent as ShieldCheckIcon } from './shield_check_icon.svg';
+export { ReactComponent as DeleteIcon } from './delete_icon.svg';
+export { ReactComponent as CallIcon } from './call_icon.svg';
